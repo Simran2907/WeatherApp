05401ed71f2514d0bc2fbf68fa3b3e62b3363828
Simran Ghotra <84228353+Simran2907@users.noreply.github.com>
2022-01-28 22:25:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#inputValue").on("keyup",function(e){
+        let cityname = e.target.value;
+        const APIKey ="621eca70dc2c9ddf388683b3f02b2b7a";
+
+        $.ajax({
+            url:`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`       
+
+        }).done(function(weatherdata){
+            console.log(weatherdata);
+            $(".profile").html(`
+            <div class="container">
+                <div class="row">
+                  <div class="card" style="width: 18rem;">
+                        <img src="http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${weatherdata.weather[0].description}</h5>
+                            <p class="card-text">The temperature at ${cityname} is = ${weatherdata.main.temp} &#8451; and it feels like it is ${weatherdata.main.feels_like} &#8451;.</p>
+                            <a href="https://www.google.com/search?q=${cityname}" class="btn btn-primary">Learn more about this state</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `);
+        })
+    });
+});
